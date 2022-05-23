d671f069518d68f1d6854d2c337a9d819f663462
Vimal Vinayraj <81632975+Vimz92@users.noreply.github.com>
2022-05-23 23:44:44 +0400

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -17,7 +17,7 @@ const Cards = () => {
   }
   return (
     <div className="container mt-3">
-      <h2 className="text-center"> Add to Cart</h2>
+      <h2 className="text-center"> Food Menu </h2>
 
       <div className="row d-flex justify-content-center align-items-center">
         {data.map((element, id) => {
